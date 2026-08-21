cafe26466117257c567d01f9bee999b1d4665373
Sri Ram <sriramreddydwarampudi>
2026-08-21 13:45:35 +0000

    fix: add missing padding import to WebViewScreen

--- app/src/main/java/com/ram/researchdesk/WebViewScreen.kt
+++ app/src/main/java/com/ram/researchdesk/WebViewScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
