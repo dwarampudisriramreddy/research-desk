a1fae387a28906a6e858c8ce7178faa6f353f260
Sri Ram <sriramreddydwarampudi>
2026-08-21 15:48:30 +0000

    fix: add missing clickable import

--- app/src/main/java/com/ram/researchdesk/HomeScreen.kt
+++ app/src/main/java/com/ram/researchdesk/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.ram.researchdesk
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
