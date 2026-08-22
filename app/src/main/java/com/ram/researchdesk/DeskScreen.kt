a802b61aa166a9d64376fd9867ec89c425a141af
Sri Ram <sriramreddydwarampudi>
2026-08-22 07:54:29 +0000

    Add missing Button import for GapsTab

--- app/src/main/java/com/ram/researchdesk/DeskScreen.kt
+++ app/src/main/java/com/ram/researchdesk/DeskScreen.kt
@@ -45,6 +45,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Button
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.HorizontalDivider
